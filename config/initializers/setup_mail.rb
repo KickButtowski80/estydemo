eed3550a9f35829bb0bd951744f4fbc2cc55bab4
kickbuttowski59 <pazpaz22@yahoo.com>
2016-09-29 02:47:34 +0000

    try to work confirmation email for sign up devise

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {    
+    :user_name             =>  'app57145901@heroku.com',
+    :password              =>  'nmhfwhv00158',
+    :domain                =>  'heroku.com',
+    :address               => 'smtp.sendgrid.net',
+    :port                  =>  '2525',
+    :authentication        =>  :plain,
+    :enable_starttls_auto  => true
+}
