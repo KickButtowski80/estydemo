d7ca8049c9015ef19da135742cff419f4ab73ae0
kickbuttowski59 <pazpaz22@yahoo.com>
2016-10-05 03:06:18 +0000

    add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :listings
   get 'pages/contact' => 'pages#contact' 
-
   get 'pages/about'=> "pages#about" 
+  get 'seller' => 'listings#seller'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
