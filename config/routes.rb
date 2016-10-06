e297a77c99f144a3712bf3842e9bac53d78c3e72
kickbuttowski59 <pazpaz22@yahoo.com>
2016-10-06 01:58:32 +0000

    add sing out route for devise.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   
   devise_for :users
+  get 'users/sign_out' => 'sessions#destroy', :as => :destroy_user_session
   resources :listings
   get 'pages/contact' => 'pages#contact' 
   get 'pages/about'=> "pages#about" 
