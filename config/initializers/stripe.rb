133b71fe4dc63f42dd4b9257bef04f3f0dc3e7dd
kickbuttowski59 <pazpaz22@yahoo.com>
2016-10-26 18:28:52 +0000

    danger

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -5,5 +5,5 @@
 
 #Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-Stripe.api_key = ENV["stripe_api_key"]
-STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
+Stripe.api_key = 'sk_test_3aZuluJM6vFjdJF5PqBWPb2d'
+STRIPE_PUBLIC_KEY = 'pk_test_v4LwI4fmO0f8ZrRfCrSJSAmt'
